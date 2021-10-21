992ee0860ceccfa3264b876b4450519815841dab
BrodyBuster <60476956+BrodyBuster@users.noreply.github.com>
2021-10-21 15:16:41 -0400

    Update fix-codec.sh

--- fix-codec.sh
+++ fix-codec.sh
@@ -24,14 +24,14 @@ NC='\033[0m'
 BLUE='\033[1;34m'
 GREEN='\033[0;32m'
 YELLOW='\033[;33m'
-LAST_START="1900-01-01 00:00:00"
+LAST_START="2000-10-18 00:00:00"
 SCRIPT=$(realpath $0)
 DATE_CREATED=$(date "+%Y-%m-%d %T")
 MOVIE_PATH=$(realpath -s $MOVIE_PATH)
 MOVIEDB=$(curl -s http://$RADARRIP:7878/radarr/api/v3/movie?apiKey=$RADARRAPI | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '^"(relativePath|audioCodec)"' 2>/dev/null)
 
 if [ -z "$MOVIEDB" ]; then
-  echo "Radarr db Empty. Please check config ... exiting"
+  echo "Radarr DB could not be retrieved. Please check config ... exiting"
   exit
 fi 
 
@@ -43,8 +43,8 @@ echo "Script last run on $LAST_START"
 
 echo "Getting movies from $MOVIE_PATH"
 declare -a FILE_LIST
-while IFS= read -u 3 -d $'\0' -r file; do
-    FILE_LIST+=( "$file" )
+while IFS= read -u 3 -d $'\0' -r FILE; do
+    FILE_LIST+=( "$FILE" )
 done 3< <(find $MOVIE_PATH -iname "*imdb-tt*" -type f -newermt "$LAST_START" -print0)
 echo "Done getting movies"
 
@@ -69,100 +69,107 @@ echo "Processing Movies ..."
 
 COUNTER=1
 for X in "${FILE_LIST[@]}"; do
-  movie=$(basename "$X")
-  imdb_tt=$(echo "$X" | grep -Po '(?<=imdb-)[^]]+')
-  radarr_codec=$(echo "$MOVIEDB" | grep "$imdb_tt" -A2 | grep "audioCodec" | cut -d '"' -f4)
+  MOVIE=$(basename "$X")
+  IMDB_TT=$(echo "$X" | grep -Po '(?<=imdb-)[^]]+')
+  RADARR_CODEC=$(echo "$MOVIEDB" | grep "$IMDB_TT" -A2 | grep "audioCodec" | cut -d '"' -f4)
   
-  if [ -z "$radarr_codec" ]; then
-     echo -e "${BLUE}$movie${NC}"
+  if [ -z "$RADARR_CODEC" ]; then
+     echo -e "${BLUE}$MOVIE${NC}"
      echo -e "[ ${RED}NOK${NC} ] Movie not found in radarr db" 
-     echo -e "$movie$ -> Not found in Radarr db" >> /tmp/fix.txt
+     echo -e "$MOVIE$ -> Not found in Radarr db" >> /tmp/fix.txt
   fi   
   
-  if [[ "$radarr_codec" == "AC3" ]]; then
-    score=$AC3
-  elif [[ "$radarr_codec" == "DTS" ]]; then
-    score=$DTS
-  elif [[ "$radarr_codec" == "DTS-ES" ]]; then
-    score=$DTSES
-  elif [[ "$radarr_codec" == "DTS-HD HRA" ]]; then
-    score=$DTSHDHRA
-  elif [[ "$radarr_codec" == "EAC3" ]]; then
-    score=$EAC3
-  elif [[ "$radarr_codec" == "EAC3 Atmos" ]]; then
-    score=$EAC3ATMOS
-  elif [[ "$radarr_codec" == "DTS-HD MA" ]]; then
-    score=$DTSHDMA
-  elif [[ "$radarr_codec" == "TrueHD" ]]; then
-    score=$TRUEHD
-  elif [[ "$radarr_codec" == "DTS-X" ]]; then
-    score=$DTSX
-  elif [[ "$radarr_codec" == "TrueHD Atmos" ]]; then
-    score=$TRUEHDATMOS
+  if [[ "$RADARR_CODEC" == "AC3" ]]; then
+    SCORE=$AC3
+  elif [[ "$RADARR_CODEC" == "DTS" ]]; then
+    SCORE=$DTS
+  elif [[ "$RADARR_CODEC" == "DTS-ES" ]]; then
+    SCORE=$DTSES
+  elif [[ "$RADARR_CODEC" == "DTS-HD HRA" ]]; then
+    SCORE=$DTSHDHRA
+  elif [[ "$RADARR_CODEC" == "EAC3" ]]; then
+    SCORE=$EAC3
+  elif [[ "$RADARR_CODEC" == "EAC3 Atmos" ]]; then
+    SCORE=$EAC3ATMOS
+  elif [[ "$RADARR_CODEC" == "DTS-HD MA" ]]; then
+    SCORE=$DTSHDMA
+  elif [[ "$RADARR_CODEC" == "TrueHD" ]]; then
+    SCORE=$TRUEHD
+  elif [[ "$RADARR_CODEC" == "DTS-X" ]]; then
+    SCORE=$DTSX
+  elif [[ "$RADARR_CODEC" == "TrueHD Atmos" ]]; then
+    SCORE=$TRUEHDATMOS
   else
-    score=0
+    SCORE=0
   fi
-  stream=1
-  audio_list=$(mediainfo "$X" "--inform=General;%Audio_Format_List%" | tr "/" "\n" |  awk '{$1=$1};1')
-  no_streams=$(echo "$audio_list" | wc -l)
-  echo "$audio_list" | while read -r line ; do
-    codec=$(echo $line)
-    if [[ "$codec" == "AC-3" ]]; then
-      codec_clean="AC3"
-      value=$AC3
-    elif [[ "$codec" == "DTS" ]]; then
-      codec_clean="DTS"
-      value=$DTS
-    elif [[ "$codec" == "DTS ES" ]] || [[ "$codec" == "DTS ES XXCH" ]] || [[ "$codec" == "DTS ES XXCH XBR" ]]; then
-      codec_clean="DTS-ES"
-      value=$DTSES
-    elif [[ "$codec" == "DTS XBR" ]]; then
-      codec_clean="DTS-HD HRA"
-      value=$DTSHDHRA
-    elif [[ "$codec" == "E-AC-3" ]]; then
-      codec_clean="EAC3"
-      value=$EAC3
-    elif [[ "$codec" == "E-AC-3 JOC" ]]; then
-      codec_clean="EAC3 Atmos"
-      value=$EAC3ATMOS
-    elif [[ "$codec" == "DTS XLL" ]] || [[ "$codec" == "DTS ES XXCH XLL" ]] || [[ "$codec" == "DTS ES XLL" ]]; then
-      codec_clean="DTS-HD MA"
-      value=$DTSHDMA
-    elif [[ "$codec" == "MLP FBA" ]]; then
-      codec_clean="TrueHD"
-      value=$TRUEHD
-    elif [[ "$codec" == "DTS XLL X" ]]; then
-      codec_clean="DTS-X"
-      value=$DTSX
-    elif [[ "$codec" == "MLP FBA 16-ch" ]]; then
-      codec_clean="TrueHD Atmos"
-      value=$TRUEHDATMOS
+  STREAM=1
+  AUDIO_LIST=$(mediainfo "$X" "--inform=General;%Audio_Format_List%" | tr "/" "\n" |  awk '{$1=$1};1')
+#  NO_STREAMS=$(echo "$AUDIO_LIST" | wc -l)
+  echo "$AUDIO_LIST" | while read -r LINE ; do
+    CODEC=$(echo $LINE)
+    if [[ "$CODEC" == "AC-3" ]]; then
+      CODEC_CLEAN="AC3"
+      VALUE=$AC3
+    elif [[ "$CODEC" == "DTS" ]]; then
+      CODEC_CLEAN="DTS"
+      VALUE=$DTS
+    elif [[ "$CODEC" == "DTS ES" ]] || [[ "$CODEC" == "DTS ES XXCH" ]] || [[ "$CODEC" == "DTS ES XXCH XBR" ]]; then
+      CODEC_CLEAN="DTS-ES"
+      VALUE=$DTSES
+    elif [[ "$CODEC" == "DTS XBR" ]]; then
+      CODEC_CLEAN="DTS-HD HRA"
+      VALUE=$DTSHDHRA
+    elif [[ "$CODEC" == "E-AC-3" ]]; then
+      CODEC_CLEAN="EAC3"
+      VALUE=$EAC3
+    elif [[ "$CODEC" == "E-AC-3 JOC" ]]; then
+      CODEC_CLEAN="EAC3 Atmos"
+      VALUE=$EAC3ATMOS
+    elif [[ "$CODEC" == "DTS XLL" ]] || [[ "$CODEC" == "DTS ES XXCH XLL" ]] || [[ "$CODEC" == "DTS ES XLL" ]]; then
+      CODEC_CLEAN="DTS-HD MA"
+      VALUE=$DTSHDMA
+    elif [[ "$CODEC" == "MLP FBA" ]]; then
+      CODEC_CLEAN="TrueHD"
+      VALUE=$TRUEHD
+    elif [[ "$CODEC" == "DTS XLL X" ]]; then
+      CODEC_CLEAN="DTS-X"
+      VALUE=$DTSX
+    elif [[ "$CODEC" == "MLP FBA 16-ch" ]]; then
+      CODEC_CLEAN="TrueHD Atmos"
+      VALUE=$TRUEHDATMOS
     else
-      codec_clean="GARBAGE"
-      value=0
+      CODEC_CLEAN="GARBAGE"
+      VALUE=0
     fi
-    if [ $stream -eq 1 ] && [[ "$codec_clean" == "$radarr_codec" ]]; then
-      echo -e "[$COUNTER OF $MOVIE_COUNT] - ${BLUE}$movie${NC}"
-      echo -e "[ ${GREEN}OK${NC} ] Audio Stream #$stream matches radarr quality" 
-      echo -e "       Radarr: $radarr_codec [$score]"
-      echo -e "       Stream #$stream: $codec_clean [$value]"
-    # This ignores the fact that radarr has a bug that identifies EAC3 7.1 as AC3 5.1  
-    elif [ $stream -eq 1 ] && [[ "AC3" == "$radarr_codec" ]] && [[ "EAC3" == "$codec_clean" ]] || [[ "EAC3 Atmos" == "$codec_clean" ]]; then
-      echo -e "[$COUNTER OF $MOVIE_COUNT] - ${BLUE}$movie${NC}"
-      echo -e "$movie$ -> Check to make sure that this is EAC3 7.1" >> /tmp/fix.txt
+    if [ $STREAM -eq 1 ] && [[ "$CODEC_CLEAN" == "$RADARR_CODEC" ]]; then
+      echo -e "[$COUNTER OF $MOVIE_COUNT] - ${BLUE}$MOVIE${NC}"
+      echo -e "[ ${GREEN}OK${NC} ] Audio Stream #$STREAM matches radarr quality" 
+      echo -e "       Radarr: $RADARR_CODEC [$SCORE]"
+      echo -e "       Stream #$STREAM: $CODEC_CLEAN [$VALUE]"
+#### This ignores the fact that radarr has a bug that identifies EAC3 7.1 as AC3 5.1  
+    elif [ $STREAM -eq 1 ] && [[ "$RADARR_CODEC" == "AC3" ]] && [[ "$CODEC_CLEAN" == "EAC3 Atmos" ]]; then
+      echo -e "[$COUNTER OF $MOVIE_COUNT] - ${BLUE}$MOVIE${NC}"
+      echo -e "$MOVIE$ -> Check to make sure that this is EAC3 7.1" >> /tmp/fix.txt
       echo -e "[ ${YELLOW}OK${NC} ] Radarr misidentifed EAC3 7.1 as AC3 5.1" 
-      echo -e "       Radarr: $radarr_codec [$score]"
-      echo -e "       Stream #$stream: $codec_clean [$value]" 
-    elif [ $score -lt $value ]; then
-      echo -e "[$COUNTER OF $MOVIE_COUNT] - ${BLUE}$movie${NC}"
-      echo -e "$movie$ -> Has better audio available" >> /tmp/fix.txt
-      echo -e "[ ${RED}NOK${NC} ] Audio Stream #$stream is higher quality than radarr" 
-      echo -e "        Stream #$stream: $codec_clean [$value]"
-      echo -e "        Radarr: $radarr_codec [$score]"
-    elif [ $score -gt $value ]; then
-      echo -e "       Stream #$stream $codec_clean [$value]"
+      echo -e "       Radarr: $RADARR_CODEC [$SCORE]"
+      echo -e "       Stream #$STREAM: $CODEC_CLEAN [$VALUE]"
+    elif [ $STREAM -eq 1 ] && [[ "$RADARR_CODEC" == "AC3" ]] && [[ "$CODEC_CLEAN" == "EAC3" ]]; then
+      echo -e "[$COUNTER OF $MOVIE_COUNT] - ${BLUE}$MOVIE${NC}"
+      echo -e "$MOVIE$ -> Check to make sure that this is EAC3 7.1" >> /tmp/fix.txt
+      echo -e "[ ${YELLOW}OK${NC} ] Radarr misidentifed EAC3 7.1 as AC3 5.1" 
+      echo -e "       Radarr: $RADARR_CODEC [$SCORE]"
+      echo -e "       Stream #$STREAM: $CODEC_CLEAN [$VALUE]" 
+#### end of radarr bug check
+    elif [ $SCORE -lt $VALUE ]; then
+      echo -e "[$COUNTER OF $MOVIE_COUNT] - ${BLUE}$MOVIE${NC}"
+      echo -e "$MOVIE$ -> Has better audio available" >> /tmp/fix.txt
+      echo -e "[ ${RED}NOK${NC} ] Audio Stream #$STREAM is higher quality than radarr" 
+      echo -e "        Stream #$STREAM: $CODEC_CLEAN [$VALUE]"
+      echo -e "        Radarr: $RADARR_CODEC [$SCORE]"
+    elif [ $SCORE -gt $VALUE ]; then
+      echo -e "       Stream #$STREAM $CODEC_CLEAN [$VALUE]"
     fi
-  stream=$((stream + 1))
+  STREAM=$((STREAM + 1))
   done
   COUNTER=$((COUNTER + 1))  
 done
